11d8a8ecd6648d10924eab3fcfc70ce7295b521a
Reed Es <reedes@users.noreply.github.com>
2023-04-17 10:12:36 -0600

    Now offers a widget/complication (#1)

---
+++ WidgetTRTP/WidgetTRTP.swift
@@ -0,0 +1,37 @@
+//
+//  WidgetTRTP.swift
+//
+// Copyright 2023  OpenAlloc LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import SwiftUI
+import WidgetKit
+
+import TroutLib
+import TroutUI
+
+struct WidgetTRTP: Widget {
+    let kind: String = "WidgetTRTP"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            WidgetView(entry: entry)
+        }
+        .configurationDisplayName("Task Routines")
+        .description("Time since last task routine.")
+        .supportedFamilies([.systemSmall])
+    }
+}
+
+struct WidgetTRTP_Previews: PreviewProvider {
+    static var previews: some View {
+        let entry = WidgetEntry(name: "Travel", imageName: nil, timeInterval: 1000, color: nil)
+        return WidgetView(entry: entry)
+            .accentColor(.blue)
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ WidgetTRTP/WidgetTRTPBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetTRTPBundle.swift
+//  WidgetTRTP
+//
+//  Created by Reed Esau on 4/14/23.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct WidgetTRTPBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetTRTP()
+    }
+}
